070f1c910b88b46d0cb8806ae5bc394a9ccb7c96
joseph_nashat <joseph_nashat@hotmail.com>
2020-07-11 20:39:17 +0200

    add debug to ios

--- src/app/cards/shared/fcm.service.ts
+++ src/app/cards/shared/fcm.service.ts
@@ -1,3 +1,5 @@
+import { AlerterService } from './alerter.service';
+import { ToasterService } from './toaster.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Platform } from '@ionic/angular';
 import { Injectable } from '@angular/core';
@@ -9,28 +11,40 @@ export class FcmService {
   constructor(
     private firebaseX: FirebaseX, // this is for cloud base messaging
     private platform: Platform,
-    private afs: AngularFirestore // we need this only for the sake of firebase database
+    private afs: AngularFirestore, // we need this only for the sake of firebase database
+    private toaster: AlerterService
   ) {}
 
   async getToken() {
-    let token: string;
     if (this.platform.is('android')) {
-      token = await this.firebaseX.getToken();
+      this.firebaseX
+        .getToken()
+        .then((token) => {
+          this.saveToken(token);
+        })
+        .catch((err) => {
+          this.toaster.presentAlert('err android: ' + err, '');
+        });
     }
     if (this.platform.is('ios')) {
-      token = await this.firebaseX.getToken();
-      const w: any = window;
-      w.FCMPlugin.requestPushPermissionIOS(
-        () => {
-          console.log('push permissions success');
-        },
-        (e) => {
-          console.log('push permissions fail', e);
-        }
-      );
-//      await this.firebaseX.grantPermission();
+      // this.toaster.presentToast('ios');
+      this.firebaseX
+        .getToken()
+        .then((token) => {
+          this.saveToken(token);
+        })
+        .catch((err) => {
+          this.toaster.presentAlert('err ios token: ' + err, '');
+        });
+      this.firebaseX
+        .grantPermission()
+        .then((granted) => this.toaster.presentAlert('granted', ''))
+        .catch((err) =>
+          this.toaster.presentAlert('err ios permission: ' + err, '')
+        );
     }
-    this.saveToken(token);
+
+    this.saveToken(null);
   }
 
   private saveToken(token: string) {
@@ -38,6 +52,7 @@ export class FcmService {
     let data = {};
     const devicesDocumentRef = this.afs.collection('devices');
     if (token) {
+      this.toaster.presentAlert('token exist', '');
       data = { token, userId: 'currentUserId' };
       devicesDocumentRef
         .doc(token)
@@ -45,6 +60,7 @@ export class FcmService {
         .then((success) => console.log(success))
         .catch((error) => console.log(error));
     } else {
+      this.toaster.presentAlert('no token exist', '');
       data = {
         token: 'ManualToken' + Date.now().toString(),
         userId: 'currentUserId',
@@ -52,8 +68,8 @@ export class FcmService {
       devicesDocumentRef
         .doc('token' + +Date.now().toString())
         .set(data)
-        .then((success) => console.log(success))
-        .catch((error) => console.log(error));
+        .then((success) => this.toaster.presentAlert('success db', ''))
+        .catch((error) => this.toaster.presentAlert('error db' + error, ''));
     }
   }
 
